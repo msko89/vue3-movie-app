91af045cfa38ad195dadf3c156a95c5f99b5dc2b
msko <kms89123@gmail.com>
2021-07-03 22:49:17 +0900

    Refactoring Search movies

--- src/store/movie.js
+++ src/store/movie.js
@@ -23,11 +23,10 @@ export default {
   actions: {
     async searchMovies({ state, getters, commit }, payload) {
       try {
-        const { title, type, year, number } = payload;
-        const OMDB_API_KEY = '7035c60c';
-        const res = await axios.get(
-          `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`
-        );
+        const res = await _fetchMovie({
+          ...payload,
+          page: 1,
+        });
 
         const { Search, totalResults } = res.data;
 
@@ -40,11 +39,12 @@ export default {
 
         if (pageLength > 1) {
           for (let page = 2; page <= pageLength; page++) {
-            if (page > number / 10) break;
+            if (page > payload.number / 10) break;
 
-            const res = await axios.get(
-              `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-            );
+            const res = await _fetchMovie({
+              ...payload,
+              page,
+            });
 
             const { Search } = res.data;
 
@@ -53,9 +53,33 @@ export default {
             });
           }
         }
-      } catch (error) {
-        alert(error);
+      } catch (message) {
+        commit('updateState', {
+          movies: [],
+          message,
+        });
       }
     },
   },
 };
+
+function _fetchMovie(payload) {
+  const { title, type, year, page } = payload;
+  const OMDB_API_KEY = '7035c60c';
+  const url = `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`;
+
+  return new Promise((resolve, reject) => {
+    axios
+      .get(url)
+      .then((res) => {
+        if (res.data.Error) {
+          reject(res.data.Error);
+        }
+
+        resolve(res);
+      })
+      .catch((error) => {
+        reject(error.message);
+      });
+  });
+}
