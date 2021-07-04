6400329c5ee81434fb09da1afed92367fedc6cca
msko <kms89123@gmail.com>
2021-07-04 12:32:56 +0900

    Movie detail

--- src/routes/Movie.vue
+++ src/routes/Movie.vue
@@ -1,17 +1,55 @@
 <template>
   <div class="container">
-    <div class="skeletons">
-      <div class="skeleton poster"></div>
+    <template v-if="loading">
+      <div class="skeletons">
+        <div class="skeleton poster"></div>
+        <div class="specs">
+          <div class="skeleton title"></div>
+          <div class="skeleton spec"></div>
+          <div class="skeleton plot"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+        </div>
+      </div>
+      <Loader :size="3" :z-index="9" fixed />
+    </template>
+    <div v-else class="movie-details">
+      <div
+        :style="{ backgroundImage: `url(${theMovie.Poster})` }"
+        class="poster"
+      ></div>
       <div class="specs">
-        <div class="skeleton title"></div>
-        <div class="skeleton spec"></div>
-        <div class="skeleton plot"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
+        <div class="title">{{ theMovie.Title }}</div>
+        <div class="labels">
+          <span>{{ theMovie.Released }}</span>
+          <span>{{ theMovie.Runtime }}</span>
+          <span>{{ theMovie.Country }}</span>
+        </div>
+        <div class="plot">
+          {{ theMovie.Plot }}
+        </div>
+        <div class="ratings">
+          <h3>Ratings</h3>
+        </div>
+        <div>
+          <h3>Actors</h3>
+          {{ theMovie.Actors }}
+        </div>
+        <div>
+          <h3>Director</h3>
+          {{ theMovie.Director }}
+        </div>
+        <div>
+          <h3>Production</h3>
+          {{ theMovie.Production }}
+        </div>
+        <div>
+          <h3>Genre</h3>
+          {{ theMovie.Genre }}
+        </div>
       </div>
     </div>
-    <Loader v-if="loading" :size="3" :z-index="9" fixed />
   </div>
 </template>
 
@@ -24,6 +62,10 @@ export default {
   },
 
   computed: {
+    theMovie() {
+      return this.$store.state.movie.theMovie;
+    },
+
     loading() {
       return this.$store.state.movie.loading;
     },
@@ -52,7 +94,6 @@ export default {
       height: 500px * 3 / 2;
       margin-right: 70px;
     }
-
     .specs {
       flex-grow: 1;
     }
@@ -65,23 +106,72 @@ export default {
         width: 80%;
         height: 70px;
       }
-
       &.spec {
         width: 60%;
         height: 30px;
         margin-top: 20px;
       }
-
       &.plot {
         width: 100%;
         height: 250px;
         margin-top: 20px;
       }
-
       &.etc {
         width: 50%;
         height: 50px;
         margin-top: 20px;
+      }
+    }
+  }
+
+  .movie-details {
+    display: flex;
+    color: $gray-600;
+
+    .poster {
+      width: 500px;
+      height: 500px * 3 / 2;
+      margin-right: 70px;
+      border-radius: 10px;
+      background-color: $gray-200;
+      background-size: cover;
+      background-position: center;
+      flex-shrink: 0;
+    }
+
+    .specs {
+      flex-grow: 1;
+
+      .title {
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 70px;
+        line-height: 1;
+        margin-bottom: 30px;
+      }
+      .labels {
+        color: $primary;
+
+        span {
+          &::after {
+            content: '\00b7';
+            margin: 0 6px;
+          }
+          &:last-child::after {
+            display: none;
+          }
+        }
+      }
+      .plot {
+        margin-top: 20px;
+      }
+      .ratings {
+      }
+      h3 {
+        margin: 24px 0 6px;
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 20px;
       }
     }
   }
